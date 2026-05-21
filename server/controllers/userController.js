66b3f83ae892df6c408cc1699d8f600fefc0a254
Chetan Nandanwar <cnandanwar289@gmail.com>
2026-05-21 22:58:31 +0530

    Add MongoDB database connection

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,9 +1,16 @@
+import User from "../models/User";
+import jwt from 'jsonwebtoken'
+import bcrypt from "bcryptjs";
 
+
+// Generate JWT
+const generateToken = (id)=>{
+    return jwt.sign({id}, process.env.JWT_SECRET, {
+        expiresIn: '30d'
+    })
+}
 
 // API to register user
-
-import User from "../models/User";
-
 export const registerUser = async (req, res) => {
     const { name, email, password } = req.body;
 
@@ -16,7 +23,40 @@ export const registerUser = async (req, res) => {
 
         const user = await User.create({name, email, password})
 
+        const token = generateToken(user._id)
+        res.json({sucess: true, token})
+
     } catch (error){
-        
+        return res.json({sucess: false, message: error.message})
+    }
+}
+
+// API to login user
+export const loginUser = async (req, res) => {
+    const { email, password } = req.body;
+
+    try{
+        const user = await User.findOne({email})
+        if(user){
+            const isMatch = await bcrypt.compare(password, user.password)
+
+            if(isMatch){
+                const token = generateToken(user._id);
+                return res.json({success: true, token})
+            }
+        }
+        return res.json({success: false, message: "Invalid email or password"})
+    } catch (error) {
+        return res.json({sucess: false, message: error.message})
+    }
+}
+
+// API to get user data
+export const getUser = async (req, res) =>{
+    try {
+        const user = req.user;
+        return res.json({success: true, user})
+    } catch (error) {
+        return res.json({success: false, message: error.message})
     }
 }
