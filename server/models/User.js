2750e63cc2015ec7750e1a69b69f7a8dc27b6b8b
Chetan Nandanwar <cnandanwar289@gmail.com>
2026-05-24 13:49:23 +0530

    corrected MongoDB database connection

--- server/models/User.js
+++ server/models/User.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema({
     name: {type: String, required: true},
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
-    Credits: {type: Number, default: 20},
+    credits: {type: Number, default: 20},
 })
 
 // Hash password before saving
